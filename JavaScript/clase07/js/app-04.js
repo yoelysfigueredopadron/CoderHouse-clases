e22f40e00747c1b6cc0e7ff991aadb586b726c62
Yoelys Figueredo Padrón <yoelys1982@gmail.com>
2023-01-19 13:26:32 -0300

    actualizando archivos js

---
+++ JavaScript/clase07/js/app-04.js
@@ -0,0 +1,14 @@
+// Funciones de orden superior
+
+// every
+// let arrayNaN = [NaN, NaN, NaN].every(isNaN);
+
+// every
+// let arrayNaN2 = [5, NaN, false].some(isNaN);
+
+// console.log(arrayNaN);
+// console.log(arrayNaN2);
+
+function mayorQue(n) {
+	return (m) => m > n;
+}
